e9ee2b11c6f35040c9fdad49986cf76bf03b56ce
wojtekcywinski <wojciech.cywinski@gmail.com>
2019-06-01 16:48:22 +0200

    Delete method made

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,14 +1,19 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import lombok.Builder;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
 
-    Task findById(Long id);
+    @Override
+    Task save(Task task);
+
+    Optional<Task> findById(Long id);
 }
 
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -16,7 +17,11 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(final Long id) {
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask(final Long id) {
         return repository.findById(id);
     }
 }
